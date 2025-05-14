d60e120266be83edf972342ca453fd6b7f50ae6b
JoeyzGit <210390879+JoeyzGit@users.noreply.github.com>
2025-05-14 15:40:40 +0530

    Just the playGame function pending now!

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
-let humanScore = 0;
-
+let humanScore = 0;    
 let computerScore = 0;
-
 
 function getComputerChoice() {
     const choice =  Math.floor(Math.random() * 3) + 1;
@@ -24,6 +22,12 @@ function playRound(humanChoice, computerChoice) {
     
     let hChoice = humanChoice.toLowerCase(); 
 
+    let humanScore = 0;    
+    let computerScore = 0;
+
+    console.log("Computer Chose : " + computerChoice);
+    console.log("You chose : " + humanChoice);
+
     // Computer Choice : Rock
     
     if(computerChoice === 'Rock') {
@@ -31,26 +35,50 @@ function playRound(humanChoice, computerChoice) {
             console.log("You Draw! Good One.");
         } else if (hChoice === 'paper') {
             console.log("Bravo! You won the round!");
+            humanScore++;
         } else {
-            console.log("You lose! Hahahahah")
+            console.log("You lose! Hahahahah Rock beats Scissors");
+            computerScore++;
         }
     }
 
-    if(computerChoice === 'Paper') {
+    else if(computerChoice === 'Paper') {
         if(hChoice === 'scissors') {
             console.log("You Win!, well done!");
+            humanScore++;
         } else if (hChoice === 'paper') {
             console.log("You Draw! Good One.");
         } else {
-            console.log("Take the L bro :)")
+            console.log("Take the L bro :) Paper beats Rock");
+            computerScore++;
         }
     }
+
+    else if(computerChoice === 'Scissors') {
+        if(hChoice === 'rock') {
+            console.log("You Win!, well done!");
+            humanScore++;
+        } else if (hChoice === 'scissors') {
+            console.log("You Draw! Good One.");
+        } else {
+            console.log("You lose! Scissors beat Paper");
+            computerScore++;
+        }
+    }
+
+    console.log("SCORES  ->  You : " + humanScore + "  |  Computer : " + computerScore);
+    console.log(" ");
 }
 
-const humanSelects = getHumanChoice();
-const computerSelects = getComputerChoice();
+function playGame() {
+}
 
-playRound(humanSelects, computerSelects);
+for(let i = 0; i < 5; i++) {
+    const humanSelects = getHumanChoice();
+    const computerSelects = getComputerChoice();
+    playRound(humanSelects, computerSelects);
+}
+
 
 // console.log(getComputerChoice());
 // console.log(getHumanChoice());
